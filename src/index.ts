48f17625046e5239705e582deb600f47d738f2b9
Billy Vong <billyvg@users.noreply.github.com>
2023-12-15 15:47:00 -0500

    add sentry

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import * as Sentry from '@sentry/browser';
+import { getCanvasManager } from '@sentry-internal/rrweb';
 import MenuScene from "./scenes/MenuScene";
 
 // set up scene
@@ -21,3 +23,31 @@ function animate(timestamp?: number) {
 
 // runs a continuous animation loop
 animate()
+
+
+Sentry.init({
+  // this assumes your build process replaces `process.env.npm_package_version` with a value
+  integrations: [
+    // If you use a bundle with performance monitoring enabled, add the BrowserTracing integration
+    new Sentry.BrowserTracing(),
+    // If you use a bundle with session replay enabled, add the SessionReplay integration
+    new Sentry.Replay({
+      useCompression: true,
+      _experiments: {
+        canvas: {
+          fps: 1,
+          quality: 0.2,
+          manager: getCanvasManager,
+        }
+      }
+    }),
+  ],
+
+  // We recommend adjusting this value in production, or using tracesSampler
+  // for finer control
+  tracesSampleRate: 0.0,
+  replaysSessionSampleRate: 1.0,
+
+  // Set `tracePropagationTargets` to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+});
